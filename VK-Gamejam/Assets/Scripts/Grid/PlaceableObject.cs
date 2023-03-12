79138d62d00416a0971c20bd1440ae1b4e9f1a66
Alexander Mosyagin <sashamos1510@gmail.com>
2023-03-12 21:27:28 +0300

    Placeable Objects Final!

--- VK-Gamejam/Assets/Scripts/Grid/PlaceableObject.cs
+++ VK-Gamejam/Assets/Scripts/Grid/PlaceableObject.cs
@@ -86,8 +86,11 @@ namespace Grid
         {
             _transform.position = point;
             _pivotPoint = new Vector2Int(x, y);
-            _sprites[0].sortingOrder = -x - y;
-            _sprites[0].sortingLayerName = layer.ToString();
+            if (_sprites.Length == 1 && _type != PlaceableTypes.Large)
+            {
+                _sprites[0].sortingOrder = -x - y;
+                _sprites[0].sortingLayerName = layer.ToString();
+            }
 
             if (_grids == null) return;
             foreach (var grid in _grids)
